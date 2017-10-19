1567e3f3400f40a24586acb3a6fdb33ac2ab069c
Trevor Gattis <trevor_gattis@cable.comcast.com>
2017-10-19 09:23:27 -0700

    Update Tabs -> spaces

--- setup.py
+++ setup.py
@@ -8,88 +8,88 @@ here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-	long_description = f.read()
+    long_description = f.read()
 
 setup(
-	name='aq',
+    name='aq',
 
-	# Versions should comply with PEP440.  For a discussion on single-sourcing
-	# the version across setup.py and the project code, see
-	# https://packaging.python.org/en/latest/single_source_version.html
-	version='0.1.2',
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1.2',
 
-	description='Query AWS resources with SQL',
-	long_description=long_description,
+    description='Query AWS resources with SQL',
+    long_description=long_description,
 
-	# The project's main homepage.
-	url='https://github.com/lebinh/aq',
+    # The project's main homepage.
+    url='https://github.com/lebinh/aq',
 
-	# Author details
-	author='Binh Le',
-	author_email='lebinh.it@gmail.com',
+    # Author details
+    author='Binh Le',
+    author_email='lebinh.it@gmail.com',
 
-	# Choose your license
-	license='MIT',
+    # Choose your license
+    license='MIT',
 
-	# See https://pypi.python.org/pypi?%3Aaction=list_classifiers
-	classifiers=[
-		# How mature is this project? Common values are
-		#   3 - Alpha
-		#   4 - Beta
-		#   5 - Production/Stable
-		'Development Status :: 3 - Alpha',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
 
-		# Indicate who your project is intended for
-		'Intended Audience :: Developers',
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
 
-		# Pick your license as you wish (should match "license" above)
-		'License :: OSI Approved :: MIT License',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
 
-		# Specify the Python versions you support here. In particular, ensure
-		# that you indicate whether you support Python 2, Python 3 or both.
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 2.6',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3',
-		'Programming Language :: Python :: 3.3',
-		'Programming Language :: Python :: 3.4',
-		'Programming Language :: Python :: 3.5',
-	],
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
 
-	# What does your project relate to?
-	keywords='aws sql query',
+    # What does your project relate to?
+    keywords='aws sql query',
 
-	# You can just specify the packages manually here if your project is
-	# simple. Or you can use find_packages().
-	packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
-	# List run-time dependencies here.  These will be installed by pip when
-	# your project is installed. For an analysis of "install_requires" vs pip's
-	# requirements files see:
-	# https://packaging.python.org/en/latest/requirements.html
-	install_requires=[
-		'boto3',
-		'docopt',
-		'pyparsing',
-		'tabulate',
-		'prompt_toolkit',
-		'pygments',
-		'six',
-		'pyyaml'
-	],
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[
+        'boto3',
+        'docopt',
+        'pyparsing',
+        'tabulate',
+        'prompt_toolkit',
+        'pygments',
+        'six',
+        'pyyaml'
+    ],
 
-	# List additional groups of dependencies here (e.g. development
-	# dependencies). You can install these using the following syntax,
-	# for example:
-	# $ pip install -e .[dev,test]
-	extras_require={
-		'dev': [],
-		'test': [],
-	},
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': [],
+        'test': [],
+    },
 
-	entry_points={
-		'console_scripts': [
-			'aq = aq:main'
-		]
-	},
+    entry_points={
+        'console_scripts': [
+            'aq = aq:main'
+        ]
+    },
 )
